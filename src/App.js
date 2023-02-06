192276036485037e0926d9bea44755b41cb062dd
Sofya <sarksofya@yandex.ru>
2023-02-06 23:03:27 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,124 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { data } from './data';
+import { gifts } from './gifts';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [giftsList, setGiftsList] = useState(0);
+  const {giftsId, giftsName, giftsImage, giftsDescription, giftsPrice, giftsSource} = gifts[giftsList];
+ console.log(gifts[giftsList]);
+
+  function previousGiftsList() {
+    setGiftsList((giftsList => {
+      giftsList--;
+
+      if (giftsList < 0) {
+        return gifts.length - 1;
+      }
+      return giftsList;
+    }));
+  }
+  const nextGiftsList = () =>{
+    setGiftsList((giftsList =>{
+      giftsList ++;
+    if(giftsList > gifts.length -1) {
+      giftsList = 0;
+    }
+       
+      return giftsList;
+      }))
+  }
+
+
+  const [babyList, setBabyList] = useState(data)
+  const [showText,setShowText] = useState(false)
+  const removeList = (id) => {
+    let newBabyList = babyList.filter(List =>List.id!==id)
+    setBabyList(newBabyList)
+  }
+  const showTextClick = (element) => {
+    element.showMore = !element.showMore;
+    setShowText(!showText)
+  }
+  return(
+  <div>
+    <div className='container'>
+  <h1 >Hello</h1>
+  </div>
+  
+  <div className='container'>
+  <img src={giftsImage} width="500px" alt="gifts"/>
+  </div>
+
+  <div className='container'>
+  <h2>{giftsId} - {giftsName}</h2>
+  <div/>
+
+  <div className='container'>
+    <h2>{giftsPrice}</h2>
+  </div>
+
+  <div className='container'>
+    <p>{giftsSource}</p>
+  </div>
+
+  <div className='container'>
+    <p>{giftsDescription}</p>
+  </div>
+
+  <div className='container'>
+    <button className='btn' onClick={previousGiftsList}>Previous</button>
+    <button className='btn' onClick={nextGiftsList}>Next</button>
+
+  </div>
+
+    
+
+
+      <div className='container'>
+        <h1>Baby Shower Gifts List {babyList.length}</h1>
+        </div>
+        {babyList.map((element => {
+        const {id, name, description, image, source, price, showMore} = element;
+        return (
+          <div key={id}>
+            <div className='container'>
+              <h2>{id} - {name}</h2>
+            </div>
+            <div className='container'>
+              <p>{source}</p>
+
+            </div>
+            <div className='container'>
+              <p>{price}</p>
+
+            </div>
+            <div className='container'>
+              <p>{showMore ? description : description.substring(0,80) + "..."}
+              <button  onClick={() => showTextClick(element)}>{showMore ? "Show less" : "Show more"}</button>
+              </p>
+
+            </div>
+            <div className='container'>
+              <img src={image} width="400px" alt="babiList"/>
+
+            </div>
+            
+            <div className='container'>
+              <button className='btn' onClick={() => removeList(id)}>Remove</button>
+            </div>
+            </div>
+           
+        )
+      }))}
+      <div className='container'>
+        <button  className='btn' onClick = {() => setBabyList([])}>Delete All</button>
+        </div> 
+ </div>
+ </div>
+  )
+  }
+ 
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,103 @@
+export const data = [
+    {
+        id: 1,
+        name: "Graco Modes Travel System",
+        description: "This 3-in-1 stroller was the top-voted stroller overall. You can use it in many different ways, including as an infant car seat carrier, a reclining baby stroller (which can be either back- or forward-facing), and a toddler stroller that can hold big and growing kids until they reach 50 pounds. This stroller has options for both a three- and five-point harness to suit your growing baby. It has a tray for you and two deep cup holders. It also has a tray for your little one and removable armrests that makes it easier to get your baby in and out.",
+        image: "https://images.ctfassets.net/9wtva4vhlgxb/45A8N9QOBHkGPYnW3qJot9/7c1d3a071bc9cf011acd21f3d9de271f/Chicco_Bravo_Trio_Travel_System__1_.jpg?fm=webp&q=70",
+        price: "Price: $255.99",
+        source: "Source: Amazon.com",
+        showMore: false
+
+    },
+    {
+        id: 2,
+        name: " Sling 'n Seat Bath Tub in Grey/Blue",
+        description: "From newborn to infant to toddler, the Fisher-Price 4-in-1 Sling 'n Seat Tub is a 4-stage bath center that adapts to your baby's growing needs. It starts out with a mesh sling to gently cradle your newborn, and turns into a roomy toddler tub.",
+        image: "https://b3h2.scene7.com/is/image/BedBathandBeyond/lf_180331064931685p_imageset?$713$&wid=713&hei=713",
+        price:  "Price: $44.99",
+        source: "Source: Fisher-Price",
+        showMore: false
+        
+    },
+    {
+        id: 3,
+        name: "Gymax 3-in-1 Baby Bassinet Beside Sleeper ",
+        description: "Soft mattress with board offers comfy sleeping experience. Safe and skin-friendly materials for secure use 2-side breathable meshes to observe baby's status. Very easy to clean with a zipper design. Sturdy construction with the firm support of steel frame. Bottom reinforced bar for enhanced stability. Movable and lockable wheels for mobility and safety. Equipped straps firmly attach to the bed. Retractable feet for getting closer to parent's bed. Keep your baby by your side with this 3-in-1 baby beside sleeper! Apart from serving as a beside sleeper, this beside crib can also act as a normal baby bassinet or a cradle. With an adjustable height of 5 levels, the beside sleeper is suitable for parents' bed of different heights. The 10° tilt can prevent your baby from spilling milk. And the cozy mattress as well as the skin-friendly border ensure comfort and security. Additionally, you can know your baby's status thanks to the breathable meshes. The movable and lockable wheels allow you to move this beside crib easily and keep stable when it is stationary.",
+        image: "https://b3h2.scene7.com/is/image/BedBathandBeyond/mktplace-s-a7f46e2f-1e72-4056-8d64-94cf7d3b7b74?$713$&wid=713&hei=713",
+        price:  "Price: $115.99",
+        source: "Source: Buybuy Baby",
+        showMore: false
+        
+    },
+    {
+        id: 4,
+        name: "4-in-1 Kickin' Tunes Music & Language Discovery Gym",
+        description: "Orchestrate the perfect playtime for your baby genius with Baby Einstein's 4-in-1 Kickin' Tunes Music & Language Discovery Gym. Adorned with various interactive toys, this mat can play over 70 melodies, sound and phrases in 3 languages.",
+        image: "https://b3h2.scene7.com/is/image/BedBathandBeyond/2021-11-03-15-05_11749_hr4in1_imageset?$713$&wid=713&hei=713",
+        price:"Price: $59.99" ,
+        source: "Source: SHOP BABY EINSTEIN ",
+        showMore: false
+        
+    },
+    {
+        id: 5,
+        name: "5-Drawer White Baby Dresser with Changing Table",
+        description: "Infinity Merch changing dresser in a white finish provides you with two very practical open storage spaces, five drawers, and a secure changer surface. Infinity Merch changing dresser in a white finish provides you with two very practical open storage spaces, five drawers, and a secure changer surface.",
+        image: "https://b3h2.scene7.com/is/image/BedBathandBeyond/mktplace-s-7ed8c8a0-8d9b-4edd-a976-ab1f30d93104?$713$&wid=713&hei=713",
+        price: "Price: $479.99" ,
+        source: "Source: SHOP BABY EINSTEIN",
+        showMore: false
+        
+    },
+    {
+        id: 6,
+        name: "Rainbow Bitzy Busy Ring™ Teething Toy",
+        description: "The textured Itzy Ritzy Rainbow Bitzy Busy Ring Teething Toy soothes and massages sore gums. Designed for children 3 months and older, the teether includes dangling toys that jingle, crinkle, and delight to stimulate your child's senses.",
+        image: "https://b3h2.scene7.com/is/image/BedBathandBeyond/2022-06-27-10-52_810434038056_02_imageset?$713$&wid=713&hei=713",
+        price: "Price: $12.99" ,
+        source: "Source: SHOP ITZY RITZY",
+        showMore: false
+        
+    },
+    {
+        id: 7,
+        name: "Bunny Sensory Toy with Teether in Cream/Multi",
+        description: "The Itzy Ritzy Bitzy Crinkle Sensory Toy makes the perfect pal to help soothe the pain caused by your child's emerging teeth. Designed with soft cotton fabric with crinkles, a braided silicone teether, and textured ribbons for engaging stimulation.",
+        image: "https://b3h2.scene7.com/is/image/BedBathandBeyond/2022-06-06-12-53_810434037912_01_imageset?$713$&wid=713&hei=713",
+        price: "Price: $14.99 " ,
+        source: "Source: SHOP ITZY RITZY",
+        showMore: false
+        
+        
+    },
+    {
+        id: 8,
+        name: "Winnie the Pooh Milestone Baby Blanket in Yellow",
+        description: "With this Winnie the Pooh Milestone Baby Blanket you can capture great photo opportunities of your growing baby. Equally plushy and soft, this blanket displays the message",
+        image: "https://b3h2.scene7.com/is/image/BedBathandBeyond/2021-09-09-18-50_085214132150_imageset?$713$&wid=713&hei=713",
+        price: "Price: $29.99" ,
+        source: "Source: SHOP DISNEY BABY",
+        showMore: false
+        
+    },
+     {
+        id: 9,
+        name: "Preemie 4-Piece Dino Take Me Home Set in Grey",
+        description: "Keep your little one cozy in the comfy cuteness of the Dino Take Me Home Set from Gerber. Crafted in oh-so-soft cotton the set includes a Sleep 'n Play Footie, a cute bodysuit with dinosaur graphic, footed pant and a coordinating cap.",
+        image: "https://b3h2.scene7.com/is/image/BedBathandBeyond/2021-04-30-23-25_op213817ek_imageset?$713$&wid=713&hei=713",
+        price: "Price: $17.99" ,
+        source: "Source: SHOP GERBER ",
+        showMore: false
+        
+    },
+     {
+        id: 10,
+        name: "Size 3M 3-Piece Sweater, Hat, and Toy Set in Grey Highrise",
+        description: "Soft chenille cardigan comes with a matching hat and easy-to-hold toy. Makes the perfect gift set",
+        image: "https://b3h2.scene7.com/is/image/BedBathandBeyond/2022-08-21-23-30_rf4a9574_imageset?$713$&wid=713&hei=713",
+        price: "Price: $26.60" ,
+        source: "Source: SHOP EVER&EVER",
+        showMore: false
+        
+    }
+]
